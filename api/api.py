cca1d8e5a5e0c1e90d4416457342687ac3c9abeb
GabrielValle91 <gvalle91@gmail.com>
2022-01-19 16:35:05 -0600

    initial setup

--- api/api.py
+++ api/api.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/fights')
 def get_fights():
-    return {"Pandamonium": ["Asphodelos: The First Circle", "Asphodelos: The Second Circle", "Asphodelos: The Third Circle", "Asphodelos: The Fourth Circle", "Asphodelos: The First Circle (Savage)", "Asphodelos: The Second Circle (Savage)", "Asphodelos: The Third Circle (Savage)", "Asphodelos: The Fourth Circle (Savage)"]}
+    return {"raids": ["Asphodelos: The First Circle", "Asphodelos: The Second Circle", "Asphodelos: The Third Circle", "Asphodelos: The Fourth Circle", "Asphodelos: The First Circle (Savage)", "Asphodelos: The Second Circle (Savage)", "Asphodelos: The Third Circle (Savage)", "Asphodelos: The Fourth Circle (Savage)"]}
